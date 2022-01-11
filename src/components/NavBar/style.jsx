508907413499e8af530496a6e601a0dd3a6b32d6
yoo chang heon <dbckdgjs1130@naver.com>
2022-01-11 19:16:58 +0900

    [feat] 네비게이션 바 생성

---
+++ src/components/NavBar/style.jsx
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled";
+
+export const Nav = styled.div`
+  width: 100%;
+  height: 60px;
+`;
+
+export const NavElement = styled.div`
+  width: 17%;
+  font-size: 20px;
+  height: 60px;
+  float: left;
+  text-align: center;
+  vertical-align: middle;
+  line-height: 60px;
+  display: inline-block;
+  overflow: hidden;
+`;
+
+export const NameElement = styled.div`
+  font-size: 15px;
+  margin-right: 20px;
+  height: 60px;
+  float: right;
+  text-align: center;
+  vertical-align: middle;
+  line-height: 60px;
+  display: inline-block;
+  overflow: hidden;
+  font-weight: bold;
+`;
+
+export const Nav2 = styled.div`
+  width: 100%;
+  height: 50px;
+  background-color: #164194;
+`;
+export const NavElement2 = styled.div`
+  margin-right: 2%;
+  margin-left: 2%;
+  display: inline-block;
+  color: white;
+  font-size: 20px;
+  height: 50px;
+  float: left;
+  text-align: center;
+  vertical-align: middle;
+  line-height: 50px;
+  font-weight: bold;
+  border-bottom: 4px solid #46a5fd;
+`;
