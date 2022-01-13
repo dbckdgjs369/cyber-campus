eb5b8f1e192fccab977adb7cc6a7aed4527ffe02
yoo chang heon <dbckdgjs1130@naver.com>
2022-01-13 22:12:10 +0900

    [refactor] css 고치기, 사용하지 않는 import 삭제

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import TodoPage from "./pages/TodoPage/Todopage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/main" element={<MainPage />} />
+        <Route path="/todo" element={<TodoPage />} />
         <Route path="/" element={<LoginPage />} />
       </Routes>
     </>
