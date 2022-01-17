517a8418b4522f4f767618958d3e547f6ac30f14
yoo chang heon <dbckdgjs1130@naver.com>
2022-01-17 22:22:09 +0900

    [fix] 로그인 이후 main화면을 todo로 하고, 강의실 route추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
 
 import { Route, Routes } from "react-router-dom";
-import MainPage from "./pages/MainPage/MainPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import TodoPage from "./pages/TodoPage/Todopage";
+import LectureRoomPage from "./pages/LectureRoomPage/LectureRoomPage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/main" element={<MainPage />} />
         <Route path="/todo" element={<TodoPage />} />
+        <Route path="/myLecture" element={<LectureRoomPage />} />
         <Route path="/" element={<LoginPage />} />
       </Routes>
     </>
