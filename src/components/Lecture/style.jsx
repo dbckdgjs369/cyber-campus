846e95772597cf40aa086517bb424e5bf17e0db6
yoo chang heon <dbckdgjs1130@naver.com>
2022-01-18 16:30:08 +0900

    [feat] 강의 컴포넌트 생성

---
+++ src/components/Lecture/style.jsx
@@ -0,0 +1,53 @@
+import styled from "@emotion/styled";
+
+export const Div = styled.div`
+  background-color: white;
+  // display: inline-block;
+  float: left;
+  // width: 22%;
+  padding: 20px;
+  // diplay: flex;
+  // flex-direction: column;
+  border: 1px solid #e7eaed;
+  border-radius: 0.25rem;
+`;
+
+export const ClassificationDiv = styled.div`
+  float: left;
+  background-color: #2e7fc9;
+  color: white;
+  border-radius: 3px;
+  font-size: 13px;
+  padding: 7px;
+  font-weight: bold;
+  align-content: center;
+  justify-content: center;
+  display: flex;
+  height: 35px;
+  width: 90px;
+`;
+
+export const Progress = styled.div`
+  backgroundcolor: #46a5fd;
+  border: 1px solid white;
+  background-color: #46a5fd;
+  list-style: none;
+  height: 15px;
+  flex-grow: 1;
+  font-size: 10px;
+`;
+export const Progressnumber = styled.div`
+  backgroundcolor: #46a5fd;
+  border: 1px solid white;
+  flex-grow: 1;
+  font-size: 10px;
+  text-align: center;
+  padding-left: 3px;
+  // jusitfy-content: flex-end;
+`;
+
+export const ProgressDiv = styled.div`
+  list-style: none;
+  margin-top: 1rem;
+  display: flex;
+`;
